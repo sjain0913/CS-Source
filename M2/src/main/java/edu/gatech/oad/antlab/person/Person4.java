dba0b98048b07fdc1bdf88b856a603ed275ba799
Saumya Jain <sjain395@gatech.edu>
2019-09-15 22:43:23 -0400

    Updated my Person4.java file with the required method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,13 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String toReturn = "";
+      for (int i = 0; i < input.length(); i ++) {
+        char x = input.charAt(i);
+        x += 1;
+        toReturn += x;
+      }
+      return toReturn;
     }
     
     /**
