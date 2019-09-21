28a8b39cc19d68304811aeffb275107ccabdc0f3
Colton Hazelton <chazelton3@gatech.edu>
2019-09-20 20:10:07 -0400

    test2

--- SilkRoad/app.py
+++ SilkRoad/app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def titleScreen():
     return render_template('TitleScreen.html')
 
+
 @app.route('/config')
 def config():
     return render_template("Config.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
