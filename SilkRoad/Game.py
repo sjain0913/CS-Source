2ba7b348752c94bdeafb64c6ea13bcd88be48842
Pritish <tarafdarpritish842@gmail.com>
2019-10-03 17:30:43 -0400

    added routing

--- SilkRoad/Game.py
+++ SilkRoad/Game.py
@@ -3,9 +3,9 @@ import Universe
 regionNames = ['China','India','Denmark','Great Britain','Egypt','Somalia','Persia','Java','Byzantium','Arabia']
 
 class Game:
-    def __init__(self, difficulty, regionNames):
+    def __init__(self, difficulty):
         self.difficulty = difficulty
-        self.regionNames=regionNames
+        self.regionNames= Game.regionNames
 
     def startGame(self):
         
