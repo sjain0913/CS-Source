0882639c93727e481ae71dfd82a6c6c2bca1640d
Joshua Donegal <jdonegal3@gatech.edu>
2019-10-04 11:34:29 -0400

    Implemented Game.py and updated stylign of Player.py and Region.py

--- SilkRoad/Game.py
+++ SilkRoad/Game.py
@@ -1,4 +1,8 @@
-import Universe
+from .Universe import Universe
+from .Player import Player
+from .Region import Region
+from .app import *
+import random
 
 regionNames = ['China','India','Denmark','Great Britain','Egypt','Somalia','Persia','Java','Byzantium','Arabia']
 
@@ -8,4 +12,13 @@ class Game:
         self.regionNames= Game.regionNames
 
     def startGame(self):
-        
+        newUniverse = Universe(regionNames)
+        regionNum = random.randint(0,10)
+        player = app.player
+        player.setRegion(Universe.regions[regionNum])
+        if (self.difficulty is 'easy'):
+            player.setCredits(1000)
+        elif (self.difficult is 'medium'):
+            player.setCredits(500)
+        else:
+            player.setCredits(100)
