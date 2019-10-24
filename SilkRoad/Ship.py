e384ab0e3a60860042c40e0ddf8668276f6fb452
Saumya Jain <samjain0913@gmail.com>
2019-10-24 17:30:31 -0400

    Added a ship python file and changed bandit encounter

---
+++ SilkRoad/Ship.py
@@ -0,0 +1,7 @@
+class Ship:
+    def __init__(self, ship_type, cargo_space, fuel_cap, health):
+        self.ship_type = ship_type
+        self.cargo_space = cargo_space
+        self.fuel_cap = fuel_cap
+        self.health = health
+    
