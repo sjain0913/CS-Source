6d176b7f54999d60331bb96077da28468d8c13cb
Pritish <tarafdarpritish842@gmail.com>
2019-10-25 12:14:07 -0400

    Added items and economy

--- SilkRoad/Region.py
+++ SilkRoad/Region.py
@@ -1,4 +1,5 @@
 from TechLevel import TechLevel
+import app
 class Region: 
     def __init__(self, name, techLevel, x, y):
         self.__techLevel = techLevel
@@ -8,19 +9,19 @@ class Region:
 
     def price_multiplier(self):
         if self.__techLevel == TechLevel(0):
-            return 0.5
+            return (0.5 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(1):
-            return 0.75
+            return (0.75 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(2):
-            return 1.0
+            return (1.0 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(3):
-            return 1.25
+            return (1.25 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(4):
-            return 1.5
+            return (1.5 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(5):
-            return 1.75
+            return (1.75 * 10.0/(app.player.barterer))
         elif self.__techLevel == TechLevel(6):
-            return 2.0
+            return (2.0 * 10.0/(app.player.barterer))
 
     def getX(self):
         return self.__x
