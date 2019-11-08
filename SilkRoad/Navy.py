1a022eb231752eef57f356e8e83ccd36d203ff48
Pritish <tarafdarpritish842@gmail.com>
2019-11-08 11:04:44 -0500

    Fixes

--- SilkRoad/Navy.py
+++ SilkRoad/Navy.py
@@ -1,12 +1,14 @@
 import random
-import app, Player, Ship
+import app
+from Player import Player
+from Ship import Ship
 class Navy:
     success = True
 
     def __init__(self, success):
         self.success = success
         self.item_list = app.player.inventory.keys()
-        self.black_listed_item = app.player.item_list[random.randint(0,len(item_list) - 1)]
+        self.black_listed_item = self.item_list[random.randint(0,len(self.item_list) - 1)]
 
     def flee(self, success):
         chance_of_success = random.randint(30, 70) * (app.player.sailor / 3)
@@ -22,8 +24,8 @@ class Navy:
             self.success = True
         else:
             self.success = False 
-            app.player.remove_from_inv(black_listed_item)
+            app.player.remove_from_inv(self.black_listed_item)
 
     def forfeit(self, item_list):
-        app.player.remove_from_inv(black_listed_item)
+        app.player.remove_from_inv(self.black_listed_item)
 
